d3b4d1f5705b08f992d75cdeb0181a44000ec813
zahari <zarchaoz@gmail.com>
2015-04-13 15:23:55 +0300

    Starting a module which will redirect non-https and non-www requests to https and www.

---
+++ examples/mod_https_www_redirect/src/lib.rs
@@ -0,0 +1,13 @@
+extern crate libc;
+
+#[macro_use]
+extern crate apache2;
+
+apache2_module!(https_www_redirect_module, https_www_redirect_handler, c_https_www_redirect_handler, b"mod_https_www_redirect\0");
+
+
+fn https_www_redirect_handler(r: &apache2::Request) -> apache2::Status {
+   r.write("https www redirect module");
+
+   apache2::Status::OK
+}
