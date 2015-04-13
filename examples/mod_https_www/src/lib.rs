86fae6e47ec9deaa39f546f0dca28562fe43a380
zahari <zarchaoz@gmail.com>
2015-04-13 18:12:25 +0300

    Renaming mod_https_www_redirect to mod_https_www.

---
+++ examples/mod_https_www/src/lib.rs
@@ -0,0 +1,13 @@
+extern crate libc;
+
+#[macro_use]
+extern crate apache2;
+
+apache2_module!(https_www_handler, c_https_www_handler, https_www_module, b"mod_https_www\0");
+
+
+fn https_www_handler(r: &apache2::Request) -> apache2::Status {
+   r.write("Redirect to https and www.");
+
+   apache2::Status::OK
+}
