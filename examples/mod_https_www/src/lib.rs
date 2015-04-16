53cd1f465c9508b457607d184e6a712309d92cae
zahari <zarchaoz@gmail.com>
2015-04-16 10:52:03 +0300

    Full implementation of the https/www redirect.

--- examples/mod_https_www/src/lib.rs
+++ examples/mod_https_www/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(collections)]
+
 #[macro_use]
 extern crate apache2;
 
@@ -5,7 +7,44 @@ apache2_module!(https_www_handler, c_https_www_handler, https_www_module, b"mod_
 
 
 fn https_www_handler(r: &mut apache2::Request) -> apache2::Status {
-   r.write("Redirect to https and www.");
+   let scheme = match r.http_scheme() {
+      None => {
+         return apache2::Status::HTTP_INTERNAL_SERVER_ERROR;
+      },
+      Some(scheme) => scheme
+   };
 
-   apache2::Status::OK
+   let hostname = match r.hostname() {
+      None => {
+         return apache2::Status::HTTP_INTERNAL_SERVER_ERROR;
+      },
+      Some(hostname) => hostname
+   };
+
+   let already_www = &hostname[..4] == "www.";
+   let already_https = scheme == "https";
+
+   if already_www && already_https {
+      return apache2::Status::DECLINED;
+   };
+
+   let full_hostname = if already_www {
+      String::from_str(hostname)
+   } else {
+      format!("www.{}", hostname)
+   };
+
+   let uri = match r.unparsed_uri() {
+      None => {
+         return apache2::Status::HTTP_INTERNAL_SERVER_ERROR;
+      },
+      Some(uri) => uri
+   };
+
+   let location = format!("https://{}{}", full_hostname, uri);
+   r.headers_out().unwrap().set("Location", location);
+
+   r.set_status(apache2::Status::HTTP_MOVED_PERMANENTLY);
+
+   apache2::Status::DONE
 }
