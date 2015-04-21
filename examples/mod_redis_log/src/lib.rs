46d49323090f319d2ded2a68cf94b59f3a6c4e0a
zahari <zarchaoz@gmail.com>
2015-04-21 13:43:30 +0300

    Hooking up the new redis log module with redis.

--- examples/mod_redis_log/src/lib.rs
+++ examples/mod_redis_log/src/lib.rs
@@ -1,10 +1,8 @@
 #[macro_use]
 extern crate apache2;
+extern crate redis;
 
-use std::io::prelude::*;
-use std::fs::File;
-use std::path::Path;
-use std::thread::sleep_ms;
+use redis::Commands;
 
 use apache2::{Request, Status, HookOrder};
 
@@ -13,15 +11,13 @@ apache2_module!(redis_log_handler, c_redis_log_handler, redis_log_module, b"mod_
 
 
 fn redis_log_handler(r: &mut Request) -> Status {
-   sleep_ms(30000);
+   let client = redis::Client::open("redis://127.0.0.1/").unwrap();
 
-   let path = Path::new("/tmp/lorem_ipsum.txt");
-
-   let mut file = File::create(&path).unwrap();
+   let con = client.get_connection().unwrap();
 
    let client_ip = r.connection().unwrap().client_ip().unwrap();
 
-   file.write_all(client_ip.as_bytes()).unwrap();
+   let _ : () = con.set("last_ip", client_ip).unwrap();
 
    Status::OK
 }
