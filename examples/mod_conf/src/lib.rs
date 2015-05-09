6f3af55214dcc01466f8a7d62a0b86ac082db818
zahari <zarchaoz@gmail.com>
2015-05-10 01:53:31 +0300

    mod_conf example_config C-struct.

--- examples/mod_conf/src/lib.rs
+++ examples/mod_conf/src/lib.rs
@@ -9,41 +9,35 @@ extern crate apache2;
 #[macro_use]
 extern crate lazy_static;
 
-use libc::{c_void, c_char};
-use apache2::{Request, Status, CmdParms, ffi};
+use libc::{c_char};
+use apache2::{Request, Status, ffi};
+use apache2::wrapper::from_char_ptr;
 
 
-struct ExampleConfig {
-   pub first_cmd: Option<String>,
-   pub second_cmd: Option<String>
-}
-
-lazy_static! {
-   static ref EXAMPLE_CONFIG: ExampleConfig = ExampleConfig {
-      first_cmd: None,
-      second_cmd: None
-   };
-}
-
-
-fn cmd_process(parms: &mut CmdParms) {
+#[repr(C)]
+pub struct example_config {
+   pub first_cmd: *const c_char,
+   pub second_cmd: *const c_char
 }
 
 
 #[allow(unused_variables)]
-pub extern "C" fn first_cmd(parms: *mut ffi::cmd_parms, mconfig: *mut c_void, w: *const c_char) -> *const c_char {
+pub extern "C" fn first_cmd(parms: *mut ffi::cmd_parms, config: *mut example_config, w: *const c_char) -> *const c_char {
+   let val = from_char_ptr(w).unwrap();
    std::ptr::null()
 }
 
 
 #[allow(unused_variables)]
-pub extern "C" fn second_cmd(parms: *mut ffi::cmd_parms, mconfig: *mut c_void, w: *const c_char) -> *const c_char {
+pub extern "C" fn second_cmd(parms: *mut ffi::cmd_parms, config: *mut example_config, w: *const c_char) -> *const c_char {
+   let val = from_char_ptr(w).unwrap();
    std::ptr::null()
 }
 
 apache2_module!(conf, b"mod_conf\0", commands {
-   AP_INIT_TAKE1!(b"FirstCmd\0", first_cmd, apache2::ffi::RSRC_CONF, b"First cmd error message\0");
-   AP_INIT_TAKE1!(b"SecondCmd\0", second_cmd, apache2::ffi::RSRC_CONF, b"Second cmd error message\0")
+   None, None, None, None,
+   AP_INIT_TAKE1!(b"FirstCmd\0", first_cmd, apache2::ffi::RSRC_CONF, b"First cmd description\0");
+   AP_INIT_TAKE1!(b"SecondCmd\0", second_cmd, apache2::ffi::RSRC_CONF, b"Second cmd description\0")
 });
 
 
