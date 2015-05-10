242e02b19028a361d52355ad85d1db6ce948ada6
zahari <zarchaoz@gmail.com>
2015-05-10 13:03:18 +0300

    Functioning mod_conf with low level ffi implementation.

--- examples/mod_conf/src/lib.rs
+++ examples/mod_conf/src/lib.rs
@@ -9,7 +9,8 @@ extern crate apache2;
 #[macro_use]
 extern crate lazy_static;
 
-use libc::{c_char};
+use std::mem;
+use libc::{c_void, c_char};
 use apache2::{Request, Status, ffi};
 use apache2::wrapper::from_char_ptr;
 
@@ -22,33 +23,71 @@ pub struct example_config {
 
 
 #[allow(unused_variables)]
-pub extern "C" fn first_cmd(parms: *mut ffi::cmd_parms, config: *mut example_config, w: *const c_char) -> *const c_char {
-   let val = from_char_ptr(w).unwrap();
+pub extern "C" fn first_cmd(parms: *mut ffi::cmd_parms, p: *mut c_void, w: *const c_char) -> *const c_char {
+   let config = unsafe { ffi::ap_get_module_config((*(*parms).server).module_config, &conf_module) as *mut example_config };
+
+   unsafe { (*config).first_cmd = w };
+
    std::ptr::null()
 }
 
 
 #[allow(unused_variables)]
-pub extern "C" fn second_cmd(parms: *mut ffi::cmd_parms, config: *mut example_config, w: *const c_char) -> *const c_char {
-   let val = from_char_ptr(w).unwrap();
+pub extern "C" fn second_cmd(parms: *mut ffi::cmd_parms, p: *mut c_void, w: *const c_char) -> *const c_char {
+   let config = unsafe { ffi::ap_get_module_config((*(*parms).server).module_config, &conf_module) as *mut example_config };
+
+   unsafe { (*config).second_cmd = w };
+
    std::ptr::null()
 }
 
+
+#[allow(unused_variables)]
+pub extern "C" fn create_server_config(p: *mut ffi::apr_pool_t, s: *mut ffi::server_rec) -> *mut c_void {
+   let config: *mut example_config = unsafe {
+      ffi::apr_pcalloc(p, mem::size_of::<example_config>() as ffi::apr_size_t) as *mut example_config
+   };
+
+   config as *mut c_void
+}
+
+
 apache2_module!(conf, b"mod_conf\0", commands {
-   None, None, None, None,
+   None,
+   None,
+   Some(create_server_config),
+   None,
    AP_INIT_TAKE1!(b"FirstCmd\0", first_cmd, apache2::ffi::RSRC_CONF, b"First cmd description\0");
    AP_INIT_TAKE1!(b"SecondCmd\0", second_cmd, apache2::ffi::RSRC_CONF, b"Second cmd description\0")
 });
 
+fn unwrap_str<'a>(wrapped: Result<&'a str, ()>) -> &'a str {
+   wrapped.unwrap_or("--")
+}
 
 fn conf_handler(r: &mut Request) -> Result<Status, ()> {
    if try!(r.handler()) != "conf" {
       return Ok(Status::DECLINED)
    }
 
+
+   let config = unsafe { ffi::ap_get_module_config(try!(try!(r.server()).module_config()).raw, &conf_module) as *mut example_config };
+
    r.set_content_type("text/plain; charset=utf-8");
 
-   try!(r.write("CONF: *****"));
+   try!(r.write("FIRST CMD: "));
+
+   let first_cmd = unwrap_str(from_char_ptr(unsafe { (*config).first_cmd }));
+   try!(r.write(first_cmd));
+
+   try!(r.write("\n"));
+
+   try!(r.write("SECOND CMD: "));
+
+   let second_cmd = unwrap_str(from_char_ptr(unsafe { (*config).second_cmd }));
+   try!(r.write(second_cmd));
+
+   try!(r.write("\n"));
 
    Ok(Status::OK)
 }
